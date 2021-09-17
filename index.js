c91fb38fb2d0663abd2d65ec69c8db9511998204
mmp17 <mirzaevmuzaffar@yahoo.com>
2021-09-17 10:35:48 +0900

    js conditional statements

---
+++ index.js
@@ -0,0 +1,73 @@
+//If Function: Yomg'ir
+
+// havo = "yomgir"
+
+// if (havo === 'issiq') {
+//     console.log('Bugun havo issiq');
+// }
+// else if (havo === 'sovuq') {
+//     console.log('Bugun havo sovuq');
+// }
+// else if (havo === 'yomgir') {
+//     console.log('Bugun havo yomgir');
+// }
+// else if (havo === 'bulut') {
+//     console.log('Bugun havo bulut');
+// }
+// else {
+//     console.log('Bunday havo turi mavjud emas');
+// }
+
+// age = 7
+
+// switch (age) {
+//     case 11: 
+//     console.log("You must be Muslima") 
+//     break;
+//     case 6: 
+//     console.log("You must be Fotima") 
+//     break;
+//     default: console.log("You must be Maryam");
+// }
+
+// weather = 'leaves'
+
+// switch (weather){
+//     case 'snow':
+//         console.log('It is winter now')
+//         break;
+//     case 'flowers':
+//         console.log('It is spring now')
+//         break;
+//     case 'hot':
+//         console.log('It is summer now')
+//         break;
+//     case 'leaves':
+//         console.log('It is fall now')
+//         break;
+// }
+
+// step = (i,name = 'September') => {
+//     console.log(name, i+', 2021');
+// };
+
+// for (let i=1; i<=30; i++){
+//     step(i)
+// }
+
+// returns:
+// September 1, 2021
+// ..
+// September 30, 2021
+
+// for i in range(1,10):
+//   for j in range(1,11):
+//     print(i,'*',j,'=',i*j,' ')
+//   console.log('\n')
+
+// Creating pifagor table:
+// for (let i=1; i<=10; i++)  
+// for (let j=1; j<=10; j++) {
+//      console.log(i,'*',j,'=',i*j)        
+//     }
+    
